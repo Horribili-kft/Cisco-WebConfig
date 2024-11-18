c2653c07343cb78bbb317a9f8e599c0c629b4083
Moricz-Flavio <moricz.flavio@students.jedlik.eu>
2024-11-18 14:24:47 +0100

    .2

--- src/components/DeviceInfo.tsx
+++ src/components/DeviceInfo.tsx
@@ -24,20 +24,27 @@ export default function DeviceInfo() {
     if (device instanceof LinuxDevice) {
         return (
             <div className="bg-base-300 rounded-lg p-4 m-4">
-                <div className='bg-error rounded-lg'>
-                    <p className='font-bold text-center'>{device.type}</p> 
+                <div className="bg-error rounded-lg pb-2">
+                    <p className="font-bold text-center text-accent-content">{device.type}</p>
                 </div>
-
-                <div className='ml-7 mt-3'>
-                <p>Hostname: {device.hostname}</p>
-                <p>Version: {device.version}</p>
-                </div>
-        
-                
-                {/* Példa linuk konfiguráció kiírására */}
+    
+                <div className="bg-base-200 shadow hover:shadow-lg hover:bg-error hover:text-white rounded-lg p-4 mt-2 transition-all duration-300">
+    <div className="flex justify-between items-center">
+        <p>Hostname:</p>
+        <div>{device.hostname}</div>
+    </div>
+</div>
+<div className="bg-base-200 shadow hover:shadow-lg hover:bg-error hover:text-white rounded-lg p-4 mt-2 transition-all duration-300">
+    <div className="flex justify-between items-center">
+        <p>Version:</p>
+        <div>{device.version}</div>
+    </div>
+</div>
             </div>
-        )
+        );
     }
+    
+    
 
 }
 
